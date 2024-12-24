5f4c913560651c8b1da0e6064eedb2cb213e5ea7
jrodbueno <jrodbueno@gmail.com>
2024-12-24 13:56:27 -0600

    Day 1 complete

--- Day01/solution.py
+++ Day01/solution.py
@@ -4,8 +4,28 @@ import os
 
 #from networkx import difference
 
-def part1(data): 
-    #define lists
+def part1(list1, list2): 
+    #Calculate accumulated difference
+    difference = 0
+    for i in range(len(list1)):
+        difference += abs(int(list1[i])-int(list2[i]))
+    return difference
+
+def part2(list1, list2):
+    #Find simmilarity score
+    sim = 0
+    for item1 in list1:
+        count = 0
+        for item2 in list2:
+            if item1 == item2:
+                count +=1
+        sim += int(item1)*count
+    return sim
+
+def parse_input(file_path):
+    with open(file_path) as f:
+        data = f.read().splitlines()
+        #define lists
     list1 = []
     list2 = []  
     #build lists
@@ -16,22 +36,9 @@ def part1(data):
     #Sort lists smallest to largest
     list1.sort()
     list2.sort()
-    #Calculate accumulated difference
-    difference = 0
-    for i in range(len(list1)):
-        difference += abs(int(list1[i])-int(list2[i]))
-    return difference
-
-def part2(data):
-    # Your solution for part 2
-    pass
-
-def parse_input(file_path):
-    with open(file_path) as f:
-        return f.read().splitlines()
+    return [list1,list2]
 
 if __name__ == "__main__":
-    #print(f"Current working directory: {os.getcwd()}")
     input_data = parse_input("Input.txt")
-    print(f"Part 1: {part1(input_data)}")
-    #print(f"Part 2: {part2(input_data)}")
+    print(f"Part 1: {part1(input_data[0],input_data[1])}")
+    print(f"Part 2: {part2(input_data[0],input_data[1])}")
