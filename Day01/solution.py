f395de4fba436407650151c3d20e960b1912be6a
jrodbueno <jrodbueno@gmail.com>
2024-12-24 13:45:02 -0600

    Final solution to day 1 part 1

--- Day01/solution.py
+++ Day01/solution.py
@@ -1,8 +1,26 @@
-import pandas as pd
+import re
+#import pandas as pd
 import os
 
+#from networkx import difference
+
 def part1(data): 
-    pass
+    #define lists
+    list1 = []
+    list2 = []  
+    #build lists
+    for item in data:
+        list_items = re.findall(r'\d+', item)
+        list1.append(list_items[0])
+        list2.append(list_items[1])
+    #Sort lists smallest to largest
+    list1.sort()
+    list2.sort()
+    #Calculate accumulated difference
+    difference = 0
+    for i in range(len(list1)):
+        difference += abs(int(list1[i])-int(list2[i]))
+    return difference
 
 def part2(data):
     # Your solution for part 2
@@ -16,4 +34,4 @@ if __name__ == "__main__":
     #print(f"Current working directory: {os.getcwd()}")
     input_data = parse_input("Input.txt")
     print(f"Part 1: {part1(input_data)}")
-    print(f"Part 2: {part2(input_data)}")
+    #print(f"Part 2: {part2(input_data)}")
