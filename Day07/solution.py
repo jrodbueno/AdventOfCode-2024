480e4aebf95942a508bb35150e112f266b91827c
jrodbueno <jrodbueno@gmail.com>
2024-12-27 17:02:23 -0600

    Input reading complete

--- Day07/solution.py
+++ Day07/solution.py
@@ -1,26 +1,41 @@
 import re
 #import pandas as pd
 import os
+import operator
 
-#from networkx import difference
+ops = {
+    '*': operator.mul,
+    '+': operator.add,
+    '-': operator.sub,
+    '/': operator.truediv
+}
+"""
+# Parse string manually
+num1, op, num2 = string[0], string[1], string[2]
+result = ops[op](int(num1), int(num2))
+print(result)  # 9
+"""
 
 def part1(input): 
+    operations = ['*', '+']
+    print(input)
     pass
 
 def part2(input):
     pass
 
 def parse_input(file_path):
-    #Read File into a list, one item per linegi
     with open(file_path) as f:
         data = f.read().splitlines()
-    #print(data)
-    #Build report list "a list of lists"
-    reports = []
+    records = []
     for item in data:
-        list_items = re.findall(r'\d+', item)
-        reports.append(list_items)
-    return reports
+        list_items = item.split(":")
+        list_items[1] = list_items[1].split(" ") # type: ignore
+        for number in list_items[1]:
+            if len(number) == 0:
+                list_items[1].pop(list_items[1].index(number))
+        records.append(list_items)
+    return records
 
 if __name__ == "__main__":
     input_data = parse_input("Input.txt")
