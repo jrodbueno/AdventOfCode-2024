c1f83774ae2c39dd39fbfe8533acc7e7b4438eec
jrodbueno <jrodbueno@gmail.com>
2024-12-26 23:48:23 -0600

    Day6 part 1 complete

--- Day06/solution.py
+++ Day06/solution.py
@@ -1,11 +1,62 @@
 import re
+import csv
 #import pandas as pd
 import os
+import sys
+sys.setrecursionlimit(150000)
 
-#from networkx import difference
+def find_guard(map,guard):
+    ver,hor = 0,0
+    for row in map:
+        if guard in row:
+            hor = row.index(guard)
+            ver = map.index(row)
+    return ver,hor
 
-def part1(input): 
-    pass
+def move (map, loc = [0,0], dir = [1,0], obstacle = '#',count=0):
+    hbound = len(map[1])-1
+    vbound = len(map)-1
+    if loc[0] + dir[0] > vbound or loc[1] + dir[1] > hbound:
+        map[loc[0]][loc[1]] = "X"
+        return "Out",loc, count
+    if map[loc[0] + dir[0]][loc[1] + dir[1]] == 'X':
+        count += 1
+        print(count)
+    if map[loc[0] + dir[0]][loc[1] + dir[1]] == obstacle:
+        dir = turn_right(dir)
+    else:
+        map[loc[0]][loc[1]] = "X"
+        loc = [loc[0] + dir[0],loc[1] + dir[1]]
+       # print(f"{loc} | {dir} | {count}")
+    return move(map,loc,dir,obstacle,count)
+
+def turn_right(direction = [-1,0]):
+    if direction == [-1,0]:
+        return [0,1]
+    if direction == [0,1]:
+        return [1,0]
+    if direction == [1,0]:
+        return [0,-1]
+    if direction == [0,-1]:
+        return [-1,0]
+    return direction
+
+def part1(map): 
+    count = 0
+    guard = '^'
+    obstacle = '#'
+    direction = [-1,0]
+    guard_loc = find_guard(map,guard)
+    movement = move(map,guard_loc,direction,obstacle, count)
+    print (movement[2])
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(map)
+    for row in map:
+        for cell in row:
+            if cell == 'X':
+                count +=1
+    return count
 
 def part2(input):
     pass
@@ -16,11 +67,10 @@ def parse_input(file_path):
         data = f.read().splitlines()
     #print(data)
     #Build report list "a list of lists"
-    reports = []
+    lines = []
     for item in data:
-        list_items = re.findall(r'\d+', item)
-        reports.append(list_items)
-    return reports
+        lines.append(list(item))
+    return lines
 
 if __name__ == "__main__":
     input_data = parse_input("Input.txt")
