929ae64f503b1a0b5aff379e1ce131425b84539c
jrodbueno <jrodbueno@gmail.com>
2024-12-29 00:40:18 -0600

    part 2 partially works

--- Day07/solution_list.py
+++ Day07/solution_list.py
@@ -3,19 +3,31 @@ import re
 import os
 import operator
 from copy import copy, deepcopy
+import csv
 
 ops = {
     '*': operator.mul,
     '+': operator.add,
     '-': operator.sub,
-    '/': operator.truediv
+    '/': operator.truediv,
+    '||': operator.concat
 }
-"""
-# Parse string manually
-num1, op, num2 = string[0], string[1], string[2]
-result = ops[op](int(num1), int(num2))
-print(result)  # 9
-"""
+
+def determine_operator_part2 (row,col,row_size,col_size):
+    #recibir una columna para saber cuántas veces tiene que haber cierto signo
+    #recibir una fila para saber cuántas veces más tiene que ir un signo
+    exponent = col_size-2
+    repeats = []
+    for i in range(exponent,0,-1):
+        repeats.append(3**i)
+    repeats.append(1)
+    if (row//repeats[col] % 3 == 0):
+        return "+"
+    if (row//repeats[col] % 3 == 1):
+        return "*"
+    else:
+        return "||"
+    
 def determine_operator (row,col,row_size,col_size):
     #recibir una columna para saber cuántas veces tiene que haber cierto signo
     #recibir una fila para saber cuántas veces más tiene que ir un signo
@@ -35,8 +47,7 @@ def part1(input):
     for row in input:
         result = row[0]
         list = row[1]
-        #Create Tree
-        print(list)
+        #print(list)
         solutions = deepcopy([list])
         i = 0
         duplicate = len(list)-1
@@ -45,7 +56,7 @@ def part1(input):
             solutions.extend(deepcopy(solutions))
             i += 1
         size = len(solutions)
-        #insert the operation in the correct place
+        #insert the operation (+/*) in the correct place
         for i, solution in enumerate(solutions):
             col = 0
             for j,item in enumerate (solution):
@@ -53,53 +64,67 @@ def part1(input):
                     col = int(j/2)
                     oper = determine_operator(i,col,size,len(list))
                     solution.insert(j,oper)  
-            #insert on 1,3,5... len-2
-            #insert with an exponencial iterative pattern
-        print(f"{size},{len(list)}")
-        print('\n'.join(map(str,solutions)))
-        
-        """
-            if len(solutions) == 0:
-                solution.append(item)
-                solutions.append(solution.copy())
-                OperatorSet = False
-            elif OperatorSet:
-                for solution in solutions:
-                    solution.append(item)
-                OperatorSet = False
-            else:
-                solutions = deepcopy(solutions)
-                solutions.extend(deepcopy(solutions))
-                len(operations)
-                i=0
-                for index, sol in enumerate(deepcopy(solutions)):
-                    solutions[index].append(operations[i])
-
-                    i = (i + 1) % len(operations)
-                OperatorSet = True
-
-        solutions = list.copy()
-        last_index = (len(solutions)-1)
-        i = 0
-        while i < last_index and i<= 10:
-            last_index = (len(solutions)-1)
-            item = solutions[i]
-            #print(item)
-            if item in operations:
-                continue
-            for operation in operations:
-                solution = list.copy()
-                solution.insert(i+1,operation)
-                solutions.append(solution)
-            i += 1
-        """
-
-                    
-                    
+        #print(f"{size},{len(list)}")
+        #print('\n'.join(map(str,solutions)))
+        for i,solution in enumerate(solutions):
+            total = 0
+            j =0
+            while j < len(solution) - 1:
+                if j == 0:
+                    total = int(solution[j])
+                total = ops[solution[j+1]](total,int(solution[j+2]))
+                j += 2
+            if total == int(result):
+                sum += total
+                break     
     return sum
 
 def part2(input):
-    pass
+    operations = ['+', '*','||']
+    sum = 0
+    for row in input:
+        result = row[0]
+        list = row[1]
+        #print(list)
+        solutions = deepcopy([list])
+        intermediate = deepcopy([list])
+        i = 0
+        duplicate = len(list)
+        #get all the possibilities
+        while i < duplicate:
+            intermediate.extend(deepcopy(solutions))
+            solutions.extend(deepcopy(intermediate))
+            i += 1
+        size = len(solutions)
+        #insert the operation (+/*) in the correct place
+        for i, solution in enumerate(solutions):
+            col = 0
+            for j,item in enumerate (solution):
+                if(j%2 == 1):
+                    col = int(j/2)
+                    oper = determine_operator_part2(i,col,size,len(list))
+                    solution.insert(j,oper)  
+        #print(f"{size},{len(list)}")
+        #print('\n'.join(map(str,solutions)))
+        for i,solution in enumerate(solutions):
+            total = 0
+            j =0
+            #print(i)
+            while j < len(solution) - 1:
+                if j == 0:
+                    total = int(solution[j])
+                if solution[j+1] == '||':
+                    total = int(ops[solution[j+1]](str(total),solution[j+2]))
+                else:
+                    total = ops[solution[j+1]](total,int(solution[j+2]))
+                j += 2
+            if total == int(result):
+                sum += total
+                break     
+        with open('path.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(solutions)
+    return sum
 
 def parse_input(file_path):
     with open(file_path) as f:
@@ -116,7 +141,7 @@ def parse_input(file_path):
 
 if __name__ == "__main__":
     input_data = parse_input("Input.txt")
-    print(f"Part 1: {part1(input_data)}")
+    #print(f"Part 1: {part1(input_data)}")
     print(f"Part 2: {part2(input_data)}")
 
 
